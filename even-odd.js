595937a237feae03c499f3ef6a4117c8d07cdb56
ahmed-ibrahem5 <ahmed.ibrahem@atis.al>
2022-04-23 15:19:01 -0700

    even-odd

---
+++ even-odd.js
@@ -0,0 +1,7 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOdd(value) {
+  return value % 2 == 0 ? "Even" : "Odd";
+}
+
+console.log(evenOdd(8));
